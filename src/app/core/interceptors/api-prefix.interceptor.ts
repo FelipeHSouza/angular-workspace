08c25c837a511c7fd0d8e75ba52906c8553f06dd
Felipe <felipejotape@icloud.com>
2020-04-16 23:11:34 -0300

    add interceptor

--- src/app/core/interceptors/api-prefix.interceptor.ts
+++ src/app/core/interceptors/api-prefix.interceptor.ts
@@ -1,2 +1,14 @@
-export class ApiPrefix {
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiPrefix implements HttpInterceptor {
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler,
+    ): Observable<HttpEvent<any>> {
+        const dupReq = req.clone({});
+        return next.handle(dupReq);
+    }
 }
